d05a7c1069007cd61a2a95916a79c4ce188e1ecb
7omahawk <ahulr.official@gmial.com>
2024-04-10 18:03:06 +0600

    added decryption

--- autoKeyCipher.py
+++ autoKeyCipher.py
@@ -8,25 +8,44 @@ string = "abcdefghijklmnopqrstuvwxyz"
 # this is the encryption part
 def encryption(userInput, key, domain, string):
     
-    cipher = ""
     # making the value of key 
     for i in range(len(key)):
         for j in range(len(string)):
             if string[j] == key[i]:
-                k = j
+                k = j   # the value of the key
+
     # using the value of key making the cipher
+    cipher = ""
     for i in range(len(userInput)):
         for j in range(len(string)):
             if string[j] == userInput[i]:
                 text = (string[(j+k)%domain])
                 cipher = cipher + text
-                k = j
+                k = j    # store next letters key
                 
     print(f"The encrypted message is: {cipher}")
     print('\n')
 
 # the decryption part will be added here
+def decryption(userInput, key, domain, string):
+    
+    cipher = ""
+    # making the value of key 
+    for i in range(len(key)):
+        for j in range(len(string)):
+            if string[j] == key[i]:
+                k = j      # the value of the key
 
+    # using the value of key making the cipher
+    for i in range(len(userInput)):
+        for j in range(len(string)):
+            if string[j] == userInput[i]:
+                text = (string[(j-k)%domain])
+                cipher = cipher + text
+                k = (j-k)%domain   # store next letters key
+                
+    print(f"The encrypted message is: {cipher}")
+    print('\n')
 
 
 # choice the what you need to do (encrypt/decrypt)
@@ -48,7 +67,7 @@ while(True):
             userInput = input("Enter your text to decrypt: ")
             key = input("Enter the key: ")
             userInput = userInput.lower()
-            # decryption(userInput, key, domain, string)
+            decryption(userInput, key, domain, string)
         elif number == 3:
             sys.exit()
         else:
